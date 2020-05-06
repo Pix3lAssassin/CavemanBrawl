a42e89689fa3fce88effdfb1367ef7a946e7690f
Justin Schreiber <th3koolperson@gmail.com>
2020-05-05 19:44:02 -0600

    Adjustments to assets

--- src/com/pixelcross/cavemanbrawl/gfx/Assets.java
+++ src/com/pixelcross/cavemanbrawl/gfx/Assets.java
@@ -27,6 +27,28 @@ public class Assets {
 		SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/town_tiles.png"));
 		SpriteSheet grassSheet = new SpriteSheet(ImageLoader.loadImage("/textures/CavemanBrawlGrassTiles.png"));
 		SpriteSheet wallSheet = new SpriteSheet(ImageLoader.loadImage("/textures/CavemanBrawlWallTiles.png"));
+		SpriteSheet playerSheet = new SpriteSheet(ImageLoader.loadImage("/textures/Caveman Paul (1).png"));
+		
+		player_down = new Image[4];
+		player_up = new Image[4];
+		player_right = new Image[4];
+		player_left = new Image[4];
+		for(int i = 0; i<4;i++)
+		{
+		player_down[i] = ImageLoader.convertToFxImage(playerSheet.crop(i*16, 0, width, height), 4);
+		}
+		for(int i = 0; i<4;i++)
+		{
+		player_up[i] = ImageLoader.convertToFxImage(playerSheet.crop(i*16, 16, width, height), 4);
+		}
+		for(int i = 0; i<4;i++)
+		{
+		player_right[i] = ImageLoader.convertToFxImage(playerSheet.crop(i*16, 32, width, height), 4);
+		}
+		for(int i = 0; i<4;i++)
+		{
+		player_left[i] = ImageLoader.convertToFxImage(playerSheet.crop(i*16, 48, width, height), 4);
+		}
 		
 		grass = new Image[8];
 		for (int y = 0; y < 2; y++) {
@@ -40,10 +62,6 @@ public class Assets {
 				walls[y*5+x] = ImageLoader.convertToFxImage(wallSheet.crop(x*width/2, y*width/2, width/2, height/2), 4);
 			}
 		}
-		
-		player_down = new Image[3];
-		
-		player_down[0] = ImageLoader.convertToFxImage(sheet.crop(0, 0, width, height), 1);
 		
 		player = ImageLoader.convertToFxImage(sheet.crop(0, 16, 16, 16), 4);
 	}
